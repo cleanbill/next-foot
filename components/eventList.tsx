8e3e729b1f0ae7a92b8270f88678548222855e5e
Mick Carter <cick.marter@gmail.com>
2022-02-26 16:39:12 +0000

    Better history event list

--- components/eventList.tsx
+++ components/eventList.tsx
@@ -107,7 +107,7 @@ export default class EventList extends Component<
           }
           .display-grid {
             display: grid;
-            grid-template-columns: 2fr 10fr;
+            grid-template-columns: 2fr 6fr;
             height: 30px;
             width: 100%;
             font-size: 14px;
